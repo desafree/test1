cf5f23c12f1448c096600b3f9c6e6b3b7ac98347
Your Name <you@example.com>
2021-12-19 17:26:52 +0100

    commit all repository

---
+++ script.js
@@ -0,0 +1,16 @@
+// const img = document.querySelector('.images .col-lg-4')
+
+
+// img.addEventListener('mouseover', ()=>{
+    
+//     setTimeout(() => {const textCont = img.querySelector('h4'); textCont.textContent = "Dopo lo smontaggio al serramento viene tolta la ferramenta e quindi sverniciato ad immersione." }, 300);
+// })
+
+
+// img.addEventListener('mouseout', ()=>{
+    
+//     setTimeout(() => {const textCont = img.querySelector('h4'); textCont.textContent = "Serramento vecchio" }, 300);
+
+
+    
+// })
